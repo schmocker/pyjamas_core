9501287399499c116f75a3707da18eccfb41ee38
agittarius <tobias.schmocker@gmail.com>
2018-12-15 16:42:35 +0100

    add gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyjamas_core",
+    version="0.0.1",
+    author="agittarius",
+    author_email="info@agittarius.ch",
+    description="Pyjamas Core Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
