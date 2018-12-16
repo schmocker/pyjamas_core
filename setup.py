b4b4e2e523e0f47d194a317d0f4ad8a08fa254cc
agittarius <tobias.schmocker@gmail.com>
2018-12-16 09:28:15 +0100

    change url to github project

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Pyjamas Core Package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/schmocker/pyjamas_core",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
